04d157ca56a4cfcf2947dfabba03a92f0c1420e8
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-14 07:45:57 +0800

    Initial commit

---
+++ wxmp/pages/home/home.js
@@ -0,0 +1,19 @@
+// pages/home/home.js
+Page({
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperList: [
+      { id: 1, img: 'https://picsum.photos/id/101/375/200', link: '' },
+      { id: 2, img: 'https://picsum.photos/id/201/375/200', link: '' },
+      { id: 3, img: 'https://picsum.photos/id/301/375/200', link: '' },
+    ]
+  },
+})
