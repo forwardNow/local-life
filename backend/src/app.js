bf26464782ea764673a19c9edca874257fed13f3
吴钦飞 <wuqinfei_csdn@163.com>
2023-06-15 09:30:04 +0800

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,9 +7,46 @@ const router = new Router();
 router.get('/', (ctx, next) => {
 });
 
+/*
+请求：
+  http://localhost:3000/getUser?id=1
+
+响应：
+  Content-Type: application/json; charset=utf-8
+
+  {
+    "id": 1,
+    "name": "张三"
+  }
+ */
 router.get('/getUser', (ctx, next) => {
-  const query = ctx.request.query; // json
-  ctx.success({ id: 1, name: '张三' + query.id });
+  const { query } = ctx.request;
+
+  // { id: '1' }
+  console.log('query: ', query);
+
+  ctx.response.body = { id: 1, name: '张三' };
+})
+
+/*
+请求：
+  http://localhost:3000/user/2
+
+响应：
+  Content-Type: application/json; charset=utf-8
+
+  {
+      "id": 2,
+      "name": "李四"
+  }
+ */
+router.get('/user/:id', (ctx, next) => {
+  const { params } = ctx;
+
+  // { id: '2' }
+  console.log('params: ', params);
+
+  ctx.response.body = { id: 2, name: '李四' };
 })
 
 app
